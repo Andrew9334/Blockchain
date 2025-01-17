5eb3e3abda4f52bfa4f4124d2db096c74b28c70f
Andrew <andfrewluferenko@icloud.com>
2025-01-17 15:06:14 +0300

    init commit

---
+++ src/main/java/com/blockchaine/blockchane/BlockchaneApplication.java
@@ -0,0 +1,13 @@
+package com.blockchaine.blockchane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchaneApplication.class, args);
+    }
+
+}
